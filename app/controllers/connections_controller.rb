e50144e8f9c26acf05b3aa1cd330dfd3ba15ac9c
caseyrriley <casey.r@rcc.tv>
2014-01-11 22:14:53 -0800

    Fixed destroy function when wanting to delete a connection, changes the styling as well.:

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -19,6 +19,7 @@ class ConnectionsController < ApplicationController
 
   # GET /connections/1/edit
   def edit
+   
   end
   def new
     @connection=Connection.new(params[:connection])
@@ -56,11 +57,12 @@ class ConnectionsController < ApplicationController
 
   # DELETE /connections/1
   # DELETE /connections/1.json
-  def destroy
-    @connection.destroy
-    respond_to do |format|
-      format.html { redirect_to connections_url }
-      format.json { head :no_content }
+ def destroy
+    connection = Connection.find(params[:id])
+    if connection.destroy
+      flash[:notice] = 'The connection was destroyed.'
+    else
+      flash[:alert] = 'The connection could not be destroyed'
     end
   end
 
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,3 +1,8 @@
 class Connection < ActiveRecord::Base
+	
 	validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+	validates :email_address, :format => /@/
+
+
 end
+ 
